b1a89d550628faafb84ad90ad5e2f02ad3754f4a
Juan Fernández Ceacero <juanf.ceacero@gmail.com>
2026-08-08 19:02:39 +0200

    build(deps): añadir fuente space mono

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router';
+import '@fontsource/space-mono/400.css';
+import '@fontsource/space-mono/700.css';
 import '@/index.css';
 import App from '@/App.jsx';
 
